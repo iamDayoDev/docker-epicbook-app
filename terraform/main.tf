ae0cf7ff3947ca08de408966bbdce389402e4697
iamDayoDev <iamdayoaderinto@gmail.com>
2026-04-29 12:31:50 +0100

    chore: add ssl cert setup to the pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,6 +74,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+    security_rule {
+    name                       = "allow-https"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "nic" {
